a818e2c6172d986850ec702cb21ffd9db6cb5075
Mark Huberty <mark.huberty@gmail.com>
2012-09-01 15:23:57 -0700

    Added topic model log directory structure and placeholders

--- code/batch_scripts/batch_r_process.sh
+++ code/batch_scripts/batch_r_process.sh
@@ -3,14 +3,14 @@
 #setwd("~/projects/twitter_election2012")
 setwd("/mnt/fwire_80/twitter_election2012")
 
-print("Acquiring data")
-source("./code/acquire/cron_search_job.R")
+# print("Acquiring data")
+# source("./code/acquire/cron_search_job.R")
 
-print("Building doc-term matrices")
-source("./code/clean/build_doc_term_mat.R")
+# print("Building doc-term matrices")
+# source("./code/clean/build_doc_term_mat.R")
 
-print("Aggregating doc-term matrices")
-source("./code/aggregate/build_scaled_sparse_tdm.R")
+# print("Aggregating doc-term matrices")
+# source("./code/aggregate/build_scaled_sparse_tdm.R")
 
 print("Generating voteshare predictions")
 source("./code/predict/voteshare_predictor_linear.R")
