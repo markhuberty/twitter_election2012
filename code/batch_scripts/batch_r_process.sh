0421d75671a11d101df306e04e7bb54888dc2c1f
Mark Huberty <mark.huberty@gmail.com>
2012-09-01 15:33:07 -0700

    Added rm/gc calls to the batch script to keep memory usage under control

--- code/batch_scripts/batch_r_process.sh
+++ code/batch_scripts/batch_r_process.sh
@@ -3,23 +3,35 @@
 #setwd("~/projects/twitter_election2012")
 setwd("/mnt/fwire_80/twitter_election2012")
 
-# print("Acquiring data")
-# source("./code/acquire/cron_search_job.R")
+print("Acquiring data")
+source("./code/acquire/cron_search_job.R")
+rm(list=ls())
+gc()
 
-# print("Building doc-term matrices")
-# source("./code/clean/build_doc_term_mat.R")
+print("Building doc-term matrices")
+source("./code/clean/build_doc_term_mat.R")
+rm(list=ls())
+gc()
 
-# print("Aggregating doc-term matrices")
-# source("./code/aggregate/build_scaled_sparse_tdm.R")
+print("Aggregating doc-term matrices")
+source("./code/aggregate/build_scaled_sparse_tdm.R")
+rm(list=ls())
+gc()
 
 print("Generating voteshare predictions")
 source("./code/predict/voteshare_predictor_linear.R")
+rm(list=ls())
+gc()
 
 print("Generating win-loss predictions")
 source("./code/predict/winloss_predictor_linear.R")
+rm(list=ls())
+gc()
 
 print("Modeling topics")
 source("./code/model/topicmodel_district.R")
+rm(list=ls())
+gc()
 
 print("Done")
 
