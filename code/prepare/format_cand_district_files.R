bb5966145dc9a28113166be50c4003aaa2ad0768
Your Name <mark.huberty@gmail.com>
2012-09-17 09:29:19 -0500

    Added the twoparty-only version of the candidates file and commented up the final code for generating it

--- code/prepare/format_cand_district_files.R
+++ code/prepare/format_cand_district_files.R
@@ -13,8 +13,15 @@ candidates <- read.csv("./data/candidates.csv",
 party.district.count <- table(candidates$state_dist, candidates$party)
 unopposed <- which(apply(party.district.count, 1, function(x) any(x == 0)))
 
-candidates.twoparty <- candidates[!(candidates$state_dist %in% names(unopposed)),]
 
+## Generate and write out the set of contested districts
+candidates.twoparty <- candidates[!(candidates$state_dist %in% names(unopposed)),]
+write.csv(candidates.twoparty,
+          file="./data/candidates.final.2012.csv",
+          row.names=FALSE
+          )
+
+## Subset the names for what we need in the district file
 cand.to.merge <- candidates.twoparty[,c("state_dist",
                                         "state_id",
                                         "district",
@@ -24,10 +31,10 @@ cand.to.merge <- candidates.twoparty[,c("state_dist",
                                         "first_name",
                                         "last_name"
                                         )
-                                     ]
-                              
+                                     ]                              
 cand.to.merge$incumbent <- as.character(cand.to.merge$incumbent)
 
+## Reshape the file into wide format
 cand.melt <- melt(cand.to.merge,
                   id.vars=c("state_dist",
                     "state_id",
@@ -35,12 +42,12 @@ cand.melt <- melt(cand.to.merge,
                     "party"
                     )
                   )
-
 cand.cast <- cast(cand.melt,
                   state_dist + state_id + district~
                   party * variable
                   )
 
+## Set the incumbent party appropriately
 cand.cast$incumbent_party <-
   ifelse(as.logical(cand.cast$R_incumbent), "R",
          ifelse(as.logical(cand.cast$D_incumbent), "D",
@@ -48,11 +55,13 @@ cand.cast$incumbent_party <-
                 )
          )
 
+## Dump the party-specific incumbency indicators
 cand.cast <- cand.cast[,!(names(cand.cast) %in% c("R_incumbent",
                                                   "D_incumbent"
                                                   )
                           )
                        ]
 
+## Write out and exit
 write.csv(cand.cast, file="./data/candidates_wide.csv", row.names=FALSE)
           
