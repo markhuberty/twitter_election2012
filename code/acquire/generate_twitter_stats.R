aef40cd9bb6007077c629d8518ef7b1978251396
Hillary Sanders <hs@berkeley.edu>
2012-09-21 09:59:08 -0700

    Changed summary stats files to .json instead of .txt.

--- code/acquire/generate_twitter_stats.R
+++ code/acquire/generate_twitter_stats.R
@@ -6,7 +6,7 @@ load("./data/cron_output/master.cron.file.RData")
 stats <- generateStats(tweets=master.cron.file, plot=FALSE)
 
 for(i in 1:length(stats)){
-  write(stats[[i]], paste("./data/summary_stats/", names(stats)[i], ".txt", sep=""))
+  write(stats[[i]], paste("./data/summary_stats/", names(stats)[i], ".json", sep=""))
 }
 
 
